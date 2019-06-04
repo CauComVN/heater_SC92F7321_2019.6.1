5be26b0fc9c52194a46dd54024380f227c555ce9
cai <cai@qq.com>
2019-06-04 16:43:27 +0800

    1.调试可控硅

--- c/PWM_Init.c
+++ c/PWM_Init.c
@@ -1,6 +1,14 @@
 #include "H/Function_Init.H"
 
-uint current_PWMDTY1=60;//6;//15; //当前功率调节PWM占空比 x/186
+uint current_PWMDTY1=0;//6;//15; //当前功率调节PWM占空比 x/186
+
+uint duty_app_time=9000; //9ms
+uint max_pwm_duty=9; //10ms 50Hz市电半个周期内9ms内最大PWM周期数目 duty_app_time*24/128/180=9000*24/128/180=9.375
+uint step_app_duty=2; //0 -- 180 调节步长
+uint curr_app_duty=30; //0 -- 180
+uint max_app_duty=180; //0 -- 180
+uint low_pwm_duty_cout=0; //m_duty个PWM周期数目为低电平
+uint curr_low_pwm_duty_count=0;
 
 //HEAT TRA PWM1 功率调节方式 flag 0:不用调节 1：增加功率 Duty增大 2：减少功率 Duty减少
 void Scr_Driver_PWM_Adjust(uint flag);
@@ -30,9 +38,16 @@ void Scr_Driver_PWM_Init(void)
 {
 	//HEAT TRA PWM1
 	
+	current_PWMDTY1=0;
+	
+	curr_low_pwm_duty_count=0;
+	low_pwm_duty_cout=(duty_app_time-50*curr_app_duty)*24/10240; // 128*180=10240
+	
+	//=========================================================================================
+	
 	//50hz 20ms 半个周期10ms 10ms/10=1ms PWM时钟为Fsys/128 187*128/24=997.33 187-1=186
 	PWMCON  = 0x16;		//PWM输出到IO，PWM时钟为Fsys/128 HEAT TRA PWM1
-	PWMPRD  = 186;		//PWM周期=(186+1)*(1*128/24us)=997.33≈=1ms;
+	PWMPRD  = 180;		//PWM周期=(186+1)*(1*128/24us)=997.33≈=1ms;
 	PWMCFG  = 0x10;//0x10;//0x00;		//PWM1输出正向,输出至P01  INV 1:输出反向 0：输出不反向 000x
 	PWMDTY1 = current_PWMDTY1;//15;     //PWM1的Duty = 15/60 =1/4
 	PWMDTYA = 0x00;		//PWM占空比微调寄存器，这里不微调
@@ -40,6 +55,45 @@ void Scr_Driver_PWM_Init(void)
 	IE1 |= 0x02;        //开启PWM中断
 	EA = 1;
 }
+
+////HEAT TRA PWM1 功率调节方式 flag 0:不用调节 1：增加功率 Duty增大 2：减少功率 Duty减少
+//void Scr_Driver_PWM_Adjust(uint flag)
+//{
+//	if(flag==1 || flag==2)
+//	{
+//		EA=0;
+//		IE1 &= 0xfd;        //关闭PWM中断		
+//		
+//		if(flag==1){ //增加功率
+//			current_PWMDTY1=current_PWMDTY1+10;
+//			if(current_PWMDTY1>185)
+//			{
+//				current_PWMDTY1=185;
+//			}
+//		}
+//		else if(flag==2)
+//		{
+//			current_PWMDTY1=current_PWMDTY1-10;
+//			if(current_PWMDTY1<1)
+//			{				
+//				current_PWMDTY1=1;
+//			}
+//		}			
+//		
+//		IE1 |= 0x02;        //开启PWM中断
+//		EA=1;
+//	}
+//}
+
+////中断处理
+//void Scr_Driver_PWMInt_Handle()
+//{
+//    //根据出水/进水温度自动调节PWM1的Duty
+//	int i=0;
+//	
+//	PWMDTY1 = current_PWMDTY1;
+//}
+
 
 //HEAT TRA PWM1 功率调节方式 flag 0:不用调节 1：增加功率 Duty增大 2：减少功率 Duty减少
 void Scr_Driver_PWM_Adjust(uint flag)
@@ -50,19 +104,23 @@ void Scr_Driver_PWM_Adjust(uint flag)
 		IE1 &= 0xfd;        //关闭PWM中断		
 		
 		if(flag==1){ //增加功率
-			current_PWMDTY1=current_PWMDTY1+10;
-			if(current_PWMDTY1>185)
+			curr_app_duty=curr_app_duty+step_app_duty;
+			if(curr_app_duty>max_app_duty)
 			{
-				current_PWMDTY1=185;
+				curr_app_duty=max_app_duty;
 			}
+			
+			low_pwm_duty_cout=(duty_app_time-50*curr_app_duty)*24/10240; // 128*180=10240
 		}
 		else if(flag==2)
 		{
-			current_PWMDTY1=current_PWMDTY1-10;
-			if(current_PWMDTY1<1)
+			curr_app_duty=curr_app_duty-step_app_duty;
+			if(curr_app_duty<1)
 			{				
-				current_PWMDTY1=1;
+				curr_app_duty=0;
 			}
+			
+			low_pwm_duty_cout=(duty_app_time-50*curr_app_duty)*24/10240; // 128*180=10240
 		}			
 		
 		IE1 |= 0x02;        //开启PWM中断
@@ -73,8 +131,21 @@ void Scr_Driver_PWM_Adjust(uint flag)
 //中断处理
 void Scr_Driver_PWMInt_Handle()
 {
-    //根据出水/进水温度自动调节PWM1的Duty
-	int i=0;
+	curr_low_pwm_duty_count++;
+	if(curr_low_pwm_duty_count<=low_pwm_duty_cout)
+	{	
+		//pwm占空比0，低电平
+		current_PWMDTY1=0;
+	}
+	else
+	{
+		//pwm占空比为180，高电平
+		current_PWMDTY1=180;
+	}
 	
-	PWMDTY1 = current_PWMDTY1;
+	if(current_PWMDTY1>max_pwm_duty)
+	{
+		//关闭pwm中断，等待下一次过零检测到来
+		IE1 &= 0xfd;        //关闭PWM中断	
+	}	
 }
--- c/main.c
+++ c/main.c
@@ -28,8 +28,8 @@ void main(void)
 			AppHandle();
 			break;
 		
-		case 0: BTM_Test();
-		break;
+//		case 0: BTM_Test();
+//		break;
 		case 1: 
 		{
 			//用P16 P17口做调功率按键中断，出水温度 进水温度替换
@@ -41,8 +41,8 @@ void main(void)
 		break;
 		case 2: Timer_Test();
 		break;
-		case 3: LCD_Test();
-		break;
+//		case 3: LCD_Test();
+//		break;
 		case 4: 
 		{
 			Scr_Driver_PWM_Test();
@@ -56,9 +56,9 @@ void main(void)
 		break;
 		case 7: ADC_Test();
 		break;
-		case 8: IAP_Test(0x1fff,IapROM);   //操作ROM，对地址0x1fff进行读写测试
-//						IAP_Test(0x7f,IapEPPROM);   //操作EPPROM，对地址0x7f进行读写测试
-		break;
+//		case 8: IAP_Test(0x1fff,IapROM);   //操作ROM，对地址0x1fff进行读写测试
+////						IAP_Test(0x7f,IapEPPROM);   //操作EPPROM，对地址0x7f进行读写测试
+//		break;
 		
 		case 9: Serial_Key_Test();
 		break;
