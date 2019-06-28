33c9091201f500dd61f57f2730b056bd10cc2b9b
Caijx <HappyCai@qq.com>
2019-06-28 18:41:03 +0800

    1.调试pid 38度 OK btm 31.3ms

--- c/BTM.c
+++ c/BTM.c
@@ -16,11 +16,11 @@ void BTM_Init(void)
 {
 //	BTMCON = 0x00;  //不开启
 //	BTMCON = 0x80;  //每15.6ms产生一个中断
-//	BTMCON = 0x81;	//每31.3ms产生一个中断
+	BTMCON = 0x81;	//每31.3ms产生一个中断
 //	BTMCON = 0x82;	//每62.5ms产生一个中断
 //	BTMCON = 0x83;	//每125ms产生一个中断
 //	BTMCON = 0x84;	//每0.25s产生一个中断
-    BTMCON = 0x85;	//每0.5s产生一个中断
+//    BTMCON = 0x85;	//每0.5s产生一个中断
 //	BTMCON = 0x86;	//每1s产生一个中断
 //	BTMCON = 0x87;	//每2s产生一个中断
     //EA = 1;           //开启总中断
